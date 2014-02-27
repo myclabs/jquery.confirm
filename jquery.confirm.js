c75477a81ebf472b5dcdd6a006b6503e941c772b
maxim <soulman.is.good@gmail.com>
2014-02-27 16:02:18 +0600

    Added global $.confirm.options, for initial application configuraion And made some fixes with direct $.confirm call without event supplied

--- jquery.confirm.js
+++ jquery.confirm.js
@@ -12,100 +12,108 @@
  */
 (function ($) {
 
-    /**
-     * Confirm a link or a button
-     * @param options {title, text, confirm, cancel, confirmButton, cancelButton, post}
-     */
-    $.fn.confirm = function (options) {
-        if (typeof options === 'undefined') {
-            options = {};
+  /**
+   * Confirm a link or a button
+   * @param options {title, text, confirm, cancel, confirmButton, cancelButton, post}
+   */
+  $.fn.confirm = function (options) {
+    if (typeof options === 'undefined') {
+      options = {};
+    }
+
+    this.click(function (e) {
+      e.preventDefault();
+
+      var newOptions = $.extend({
+        button: $(this)
+      }, options);
+
+      $.confirm(newOptions, e);
+    });
+
+    return this;
+  };
+
+  /**
+   * Show a confirmation dialog
+   * @param options {title, text, confirm, cancel, confirmButton, cancelButton, post}
+   */
+  $.confirm = function (options, e) {
+    // Default options
+    var settings = $.extend($.confirm.options, {
+      confirm: function (o) {
+        var url = e && (('string' === typeof e && e) || (e.currentTarget && e.currentTarget.attributes['href'].value));
+        if(url) {
+          if (options.post) {
+            var form = $('<form method="post" class="hide" action="' + url + '"></form>');
+            $("body").append(form);
+            form.submit();
+          } else {
+            window.location = url;
+          }
         }
+      },
+      cancel: function (o) {
+      }
+    },options);
 
-        this.click(function (e) {
-            e.preventDefault();
+    // Modal
+    var modalHeader = '';
+    if (settings.title !== '') {
+      modalHeader =
+        '<div class=modal-header>' +
+          '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+          '<h4 class="modal-title">' + settings.title+'</h4>' +
+          '</div>';
+    }
+    var modalHTML =
+      '<div class="confirmation-modal modal fade" tabindex="-1" role="dialog">' +
+        '<div class="modal-dialog">' +
+        '<div class="modal-content">' +
+        modalHeader +
+        '<div class="modal-body">' + settings.text + '</div>' +
+        '<div class="modal-footer">' +
+        '<button class="confirm btn btn-primary" type="button" data-dismiss="modal">' +
+        settings.confirmButton +
+        '</button>' +
+        '<button class="cancel btn btn-default" type="button" data-dismiss="modal">' +
+        settings.cancelButton +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
 
-            var newOptions = $.extend({
-                button: $(this)
-            }, options);
+    var modal = $(modalHTML);
 
-            $.confirm(newOptions, e);
-        });
+    modal.on('shown', function () {
+      modal.find(".btn-primary:first").focus();
+    });
+    modal.on('hidden', function () {
+      modal.remove();
+    });
+    modal.find(".confirm").click(function () {
+      settings.confirm(settings.button);
+    });
+    modal.find(".cancel").click(function () {
+      settings.cancel(settings.button);
+    });
 
-        return this;
-    };
+    // Show the modal
+    $("body").append(modal);
+    modal.modal('show');
+  };
 
-    /**
-     * Show a confirmation dialog
-     * @param options {title, text, confirm, cancel, confirmButton, cancelButton, post}
-     */
-    $.confirm = function (options, e) {
-        // Default options
-        var settings = $.extend({
-            text: "Are you sure?",
-            title: "",
-            confirmButton: "Yes",
-            cancelButton: "Cancel",
-            post: false,
-            confirm: function (o) {
-                var url = e.currentTarget.attributes['href'].value;
-                if (options.post) {
-                    var form = $('<form method="post" class="hide" action="' + url + '"></form>');
-                    $("body").append(form);
-                    form.submit();
-                } else {
-                    window.location = url;
-                }
-            },
-            cancel: function (o) {
-            },
-            button: null
-        }, options);
-
-        // Modal
-        var modalHeader = '';
-        if (settings.title !== '') {
-            modalHeader =
-                '<div class=modal-header>' +
-                    '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-                    '<h4 class="modal-title">' + settings.title+'</h4>' +
-                '</div>';
-        }
-        var modalHTML =
-                '<div class="confirmation-modal modal fade" tabindex="-1" role="dialog">' +
-                    '<div class="modal-dialog">' +
-                        '<div class="modal-content">' +
-                            modalHeader +
-                            '<div class="modal-body">' + settings.text + '</div>' +
-                            '<div class="modal-footer">' +
-                                '<button class="confirm btn btn-primary" type="button" data-dismiss="modal">' +
-                                    settings.confirmButton +
-                                '</button>' +
-                                '<button class="cancel btn btn-default" type="button" data-dismiss="modal">' +
-                                    settings.cancelButton +
-                                '</div>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>' +
-                '</div>';
-
-        var modal = $(modalHTML);
-
-        modal.on('shown', function () {
-            modal.find(".btn-primary:first").focus();
-        });
-        modal.on('hidden', function () {
-            modal.remove();
-        });
-        modal.find(".confirm").click(function () {
-            settings.confirm(settings.button);
-        });
-        modal.find(".cancel").click(function () {
-            settings.cancel(settings.button);
-        });
-
-        // Show the modal
-        $("body").append(modal);
-        modal.modal('show');
-    };
-
+  /**
+   * Globally definable rules
+   * @type {{text: string, title: string, confirmButton: string, cancelButton: string, post: boolean, confirm: Function, cancel: Function, button: null}}
+   */
+  $.confirm.options = {
+    text: "Are you sure?",
+    title: "",
+    confirmButton: "Yes",
+    cancelButton: "Cancel",
+    post: false,
+    button: null
+  }
 })(jQuery);
