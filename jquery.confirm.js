c680ea096b8e221be31b5431975cdeda1f66b1f9
Matthieu Napoli <matthieu@mnapoli.fr>
2015-03-20 13:32:40 +1300

    #45 Add missing quotes

--- jquery.confirm.js
+++ jquery.confirm.js
@@ -87,7 +87,7 @@
         var modalHeader = '';
         if (settings.title !== '') {
             modalHeader =
-                '<div class=modal-header>' +
+                '<div class="modal-header">' +
                     '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
                     '<h4 class="modal-title">' + settings.title+'</h4>' +
                 '</div>';
