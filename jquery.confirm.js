b30e9250d8a4d165d4c756fecb5daf31f4ffdf0f
Deuteu <Deuteu@users.noreply.github.com>
2016-10-02 15:46:44 +0200

    Add no Cancel button option.

--- jquery.confirm.js
+++ jquery.confirm.js
@@ -101,6 +101,13 @@
                     '<h4 class="modal-title">' + settings.title+'</h4>' +
                 '</div>';
         }
+        var cancelButtonHtml = '';
+        if (settings.cancelButton) {
+            cancelButtonHtml =
+                '<button class="cancel btn ' + settings.cancelButtonClass + '" type="button" data-dismiss="modal">' +
+                    settings.cancelButton +
+                '</button>'
+        }
         var modalHTML =
                 '<div class="confirmation-modal modal fade" tabindex="-1" role="dialog">' +
                     '<div class="'+ settings.dialogClass +'">' +
@@ -111,9 +118,7 @@
                                 '<button class="confirm btn ' + settings.confirmButtonClass + '" type="button" data-dismiss="modal">' +
                                     settings.confirmButton +
                                 '</button>' +
-                                '<button class="cancel btn ' + settings.cancelButtonClass + '" type="button" data-dismiss="modal">' +
-                                    settings.cancelButton +
-                                '</button>' +
+                                cancelButtonHtml +
                             '</div>' +
                         '</div>' +
                     '</div>' +
